1dde95911c41ecf8f07716f50d9ba40d2214cc74
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-02-13 16:25:15 +0300

    second commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+1. In the first commit a project with an empty activity was created.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+2. In the second commit, a text field and a button were added.  
+Then the following functionality was added: we enter text and by clicking on the button we display a message with the text entered.  
+If there is no text in the field, then nothing happens by pressing the button.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.homework;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputText;
+    private Button mInputButton;
+    private View.OnClickListener mInputButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String s1 = mInputText.getText().toString();
+            if (!isInputValid()){
+                Toast.makeText(getApplicationContext(), s1, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+    private boolean isInputValid (){
+        return TextUtils.isEmpty(mInputText.getText());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInputText = findViewById(R.id.input_edit);
+        mInputButton = findViewById(R.id.input_button);
+        mInputButton.setOnClickListener(mInputButtonClickListener);
     }
 }
